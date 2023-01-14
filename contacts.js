8b2979ebb90b348cb313eb3f3675de6fd4e154f4
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2023-01-14 18:32:47 +0200

    Interim commit

---
+++ contacts.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json')
+
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then(data => console.log(JSON.parse(data)))
+    .catch(err => console.log(err.message));
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then(data => JSON.parse(data))
+    .then(contacts => console.log(contacts.find(contact => contact.id === contactId.toString())))
+    .catch(err => console.log(err.message))
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath)
+    .then(data => JSON.parse(data))
+    .then(contacts => contacts.filter(contact => contact.id !== contactId.toString()))
+    .then(updatedContacts => fs.writeFile(contactsPath, JSON.stringify(updatedContacts), err => {
+      if (err) {
+        console.error(err);
+      };
+    }))
+    .catch(err => console.log(err.message))
+}
+
+function addContact(name, email, phone) {
+
+  let newContact = {
+    id: '0',
+    name: name.toString(),
+    email: email.toString(),
+    phone: phone.toString()
+  }
+
+  fs.readFile(contactsPath)
+    .then(data => JSON.parse(data))
+    .then(contacts => contacts.map(contact => parseInt(contact.id)))
+    .then(allId => Math.max(...allId) + 1)
+    .then(id => newContact.id = id.toString())
+    .then(() => fs.readFile(contactsPath))
+    .then(contactBeforeUpdate => JSON.parse(contactBeforeUpdate))
+    .then(prevContacts => fs.writeFile(contactsPath, JSON.stringify([...prevContacts, newContact]), err => {
+      if (err) {
+        console.error(err);
+      };
+    }))
+    .catch(err => console.log(err.message))
+}
+
+
+
+
+module.exports = { listContacts, getContactById, removeContact, addContact }
---
+++ index.js
@@ -0,0 +1,9 @@
+const { listContacts } = require('./contacts');
+console.log(testdata)
+
+// Зроби імпорт модуля contacts.js в файлі index.js та перевір працездатність функції для роботи з контактами.
+
+// https://www.edu.goit.global/uk/learn/5265254/2439558/2439561/homework
+// https://www.youtube.com/watch?v=3aGSqasVPsI
+// https://www.youtube.com/watch?v=TVINW3atTE0&t=4939s
+// https://youtu.be/yWgcVhwr3Ak
